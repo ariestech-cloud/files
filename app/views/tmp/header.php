ff891107d5fea71fcb3b97b36462389483c7d122
ariestech-cloud <ariessilver645@gmail.com>
2024-09-04 22:51:01 +0700

    Update header.php

--- app/views/tmp/header.php
+++ app/views/tmp/header.php
@@ -5,9 +5,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-utilities.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.8/codemirror.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.8/theme/dracula.min.css">
-        <link rel="stylesheet"
+    <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Matemasie">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap.min.css">
 <link rel="stylesheet" href="<?=$this->base_url();?>assets/css/style.css">
